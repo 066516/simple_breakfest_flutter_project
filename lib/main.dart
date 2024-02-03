f6182e83f617bed997073ddceb4e7ac8f4d14229
066516 <ghmamdjeridinabil@gmail.com>
2024-02-04 00:00:37 +0100

    dd catogries

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of    application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: home(),
+      home: Home(),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,33 +1,121 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:simple_breakfest_flutter_project/models/category.dart';
 
-class home extends StatelessWidget {
-  const home({super.key});
+class Home extends StatefulWidget {
+  Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<CategoryModel> categories = [];
+
+  void _getCategries() {
+    categories = CategoryModel.getCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _getCategries();
     return Scaffold(
       appBar: appBar(),
-      body: Column(children: [
-        Container(
-            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-            decoration: BoxDecoration(boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.11),
-                blurRadius: 40,
-                spreadRadius: 0.0,
-              )
-            ]),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-              ),
-            ))
+      backgroundColor: Colors.white,
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        _SearchBox(),
+        SizedBox(
+          height: 40,
+        ),
+        _CategoriesSection()
       ]),
     );
+  }
+
+  Column _CategoriesSection() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: EdgeInsets.only(left: 20),
+        child: Text(
+          'Category',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Container(
+        height: 150,
+        // color: Colors.green,
+        child: ListView.separated(
+            itemCount: categories.length,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            separatorBuilder: (context, index) => const SizedBox(width: 25),
+            itemBuilder: (context, index) {
+              return Container(
+                height: 50,
+                width: 120,
+                decoration: BoxDecoration(
+                    color: categories[index].boxColor.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white, shape: BoxShape.circle),
+                      child: SvgPicture.asset(
+                        categories[index].iconPath,
+
+                        // width: 50,
+                        // height: 50,
+                      ),
+                    ),
+                    Text(
+                      categories[index].name,
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18),
+                    )
+                  ],
+                ),
+              );
+            }),
+      )
+    ]);
+  }
+
+  Container _SearchBox() {
+    return Container(
+        margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.11),
+            blurRadius: 40,
+            spreadRadius: 0.0,
+          ),
+        ]),
+        child: TextField(
+          decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              prefixIcon: Icon(Icons.search),
+              suffixIcon: Icon(
+                Icons.filter_list,
+              ),
+              hintText: "Search for products or brands",
+              hintStyle: TextStyle(color: Colors.grey),
+              contentPadding: EdgeInsets.all(15),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none)),
+        ));
   }
 
   AppBar appBar() {
